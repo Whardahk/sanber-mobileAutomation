dd456101028a968ebd7e9f52732b3c53a6216170
Whardah <whardah25@gmail.com>
2022-12-26 19:58:25 +0700

    Receive result feature

--- test/specs/mobile.automation.js
+++ test/specs/mobile.automation.js
@@ -9,4 +9,19 @@ describe("Open Application", () => {
       ).toHaveAttribute("text", "Hello World")
     );
   });
+
+  it("should open Receive Result Activity", async () => {
+    await $("//android.widget.TextView[@content-desc='App']").click();
+    await $("//android.widget.TextView[@content-desc='Activity']").click();
+    await $(
+      "//android.widget.TextView[@content-desc='Receive Result']"
+    ).click();
+    await $("//android.widget.Button[@content-desc='Get Result']").click();
+    await $("//android.widget.Button[@content-desc='Violet']").click();
+    await expect(
+      $(
+        "/hierarchy/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.LinearLayout/android.widget.TextView[2]"
+      ).toHaveAttribute("text", "(okay -1) Violet!")
+    );
+  });
 });
